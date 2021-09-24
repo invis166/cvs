5b1bcba6dd9e3a93ab159ca64f48dd057c0c4302
invis166 <kolesnikovv028@gmail.com>
2021-09-24 21:16:25 +0500

    add basic test functionality

--- modules/interpreter.py
+++ modules/interpreter.py
@@ -1,2 +1,14 @@
-class Interpretator:
-    pass
+import cmd
+
+
+class CVSShell(cmd.Cmd):
+    intro = 'test'
+    prompt = '> '
+
+    #----- basic cvs commands -----
+    def do_init(self, arg):
+        print(f'{arg=}')
+
+
+if __name__ == '__main__':
+    CVSShell().cmdloop()
