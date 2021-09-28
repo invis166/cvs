14c455b28f95cddb7ae1108eacadeb66d70be77d
invis166 <kolesnikovv028@gmail.com>
2021-09-28 21:07:31 +0500

    add Head and Branch classes and expand add method for initializing from reference in CVSObject metaclass

--- modules/cvs_objects.py
+++ modules/cvs_objects.py
@@ -1,12 +1,17 @@
 import abc
+import os
 
 
 class CVSObject(metaclass=abc.ABCMeta):
     def get_hash(self) -> bytes:
-        return b'hash'
+        pass
 
     def get_content(self) -> bytes:
-        return b'content'
+        pass
+
+    @staticmethod
+    def initialize_from_reference(path_to_reference: str) -> "CVSObject":
+        pass
 
 
 class Blob(CVSObject):
@@ -26,16 +31,43 @@ class Commit(CVSObject):
 
         return commit
 
+    @staticmethod
+    def initialize_from_reference(path_to_reference: str) -> "Commit":
+        pass
+
 
 class Tree(CVSObject):
     def __init__(self):
         self.children = {}
 
     def add_item(self, item_hash: bytes, item_name: str, item_type: str):
-        self.children[item_hash] = TreeObjectData(item_name, item_type)
+        self.children[item_hash] = _TreeObjectData(item_name, item_type)
 
 
-class TreeObjectData:
+class _TreeObjectData:
     def __init__(self, name: str, object_type: str):
         self.name = name
         self.object_type = object_type
+
+
+class Branch(CVSObject):
+    def __init__(self, name: str, commit: Commit):
+        self.name = name
+        self.commit = commit
+
+    @staticmethod
+    def initialize_from_reference(path_to_reference: str) -> "Branch":
+        branch_name = os.path.basename(path_to_reference)
+        with open(path_to_reference, 'r') as ref:
+            commit_ref_dir = ref.readline()[0][:5]  # removing 'ref: ' prefix
+
+        return Branch(branch_name, Commit.initialize_from_reference(commit_ref_dir))
+
+
+class Head(CVSObject):
+    def __init__(self, branch: Branch):
+        self.branch = branch
+
+    @staticmethod
+    def initialize_from_reference(path_to_reference: str) -> "Head":
+        pass
