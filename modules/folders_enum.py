d78ef0a9b4b9718d510de813e2d5028d7b4401fb
invis166 <kolesnikovv028@gmail.com>
2021-09-30 01:50:42 +0500

    change folders paths from relative to real(start from CVS DATA)

--- modules/folders_enum.py
+++ modules/folders_enum.py
@@ -3,6 +3,6 @@ from enum import Enum
 
 class FoldersEnum(str, Enum):
     CVS_DATA = '.cool_cvs/'
-    REFS = 'refs/'
-    HEADS = 'refs/heads'
-    OBJECTS = 'objects/'
+    REFS = '.cool_cvs/refs/'
+    HEADS = '.cool_cvs/refs/heads'
+    OBJECTS = '.cool_cvs/objects/'
