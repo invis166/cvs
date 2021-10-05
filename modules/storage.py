b9a2e76e0047df6709ea37b08e6dbe1f71a3e7d6
invis166 <kolesnikovv028@gmail.com>
2021-10-06 01:01:25 +0500

    add KVStorage interface and implement FolderStorage

--- modules/storage.py
+++ modules/storage.py
@@ -1,32 +1,47 @@
 import os
+import abc
 
-from folders_enum import FoldersEnum
 from cvs_objects import CVSObject
 
 
-class KVStorage:
+class KVStorage(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def store(self, item):
+        pass
+
+    @abc.abstractmethod
+    def read(self, data):
+        pass
+
+
+class FolderStorage(KVStorage):
     def __init__(self, directory: str):
         self.directory = directory
-        self.path_to_objects = os.path.join(self.directory, FoldersEnum.OBJECTS)
 
     def store(self, item: CVSObject):
-        item_hash = item.get_hash()
+        item_hash = item.get_object_hash()
         object_name = item_hash.hex()[2:]
 
-        object_directory = KVStorage.get_object_directory(self.path_to_objects, item_hash)
+        object_directory = FolderStorage.get_object_directory(self.directory, item_hash)
         os.makedirs(object_directory)
 
         with open(os.path.join(object_directory, object_name), 'wb') as f:
-            f.write(item.get_content())
+            f.write(item.serialize())
 
-    def read_from_hash(self, item_hash: bytes) -> bytes:
+    def read(self, item_hash: bytes) -> bytes:
         object_name = item_hash.hex()[2:]
+        object_directory = FolderStorage.get_object_directory(self.directory, item_hash)
 
-        object_directory = KVStorage.get_object_directory(self.path_to_objects, item_hash)
-        with open(os.path.join(object_directory, object_name), 'rb') as f:
-            return f.read()
+        return FolderStorage.get_file_content(os.path.join(object_directory, object_name))
 
     @staticmethod
     def get_object_directory(path_to_objects: str, object_hash: bytes) -> str:
         return os.path.join(path_to_objects, object_hash.hex()[:2])
+
+    @staticmethod
+    def get_file_content(path: str):
+        with open(path, 'rb') as f:
+            return f.read()
+
+
 
