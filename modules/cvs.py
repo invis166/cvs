5194c3d5dfc835e6e51c1de5b358d17e597d8b47
invis166 <kolesnikovv028@gmail.com>
2021-10-23 19:32:40 +0500

    add update_index function

--- modules/cvs.py
+++ modules/cvs.py
@@ -1,9 +1,10 @@
 import os
 from dataclasses import dataclass
 
-from cvs_objects import CVSObject, Commit, Tree, Blob, Branch, Head, TreeObjectData
-from storage import CVSStorage
-from folders_enum import FoldersEnum
+from modules.cvs_objects import CVSObject, Commit, Tree, Blob, TreeObjectData
+from modules.references import Branch, Head
+from modules.storage import CVSStorage
+from modules.folders_enum import FoldersEnum
 
 
 class CVS:
@@ -19,7 +20,7 @@ class CVS:
         if CVS.is_repository_exists(self.path_to_repository):
             # Just initializing refs
             self._initialize_head()
-            self.update_index()
+            self.index.update_index()
             return
 
         # Creating internal files and directories
@@ -30,79 +31,86 @@ class CVS:
         with open(os.path.join(self.path_to_repository, 'HEAD'), 'w'):
             pass
 
-        self.update_index()
-
-    def update_index(self):
-        '''Compare current repository state to last commit and add new or changed or removed files to index'''
-        pass
+        self.index.update_index()
 
     @staticmethod
     def is_repository_exists(path_to_repository: str) -> bool:
         path_to_cvs_data = os.path.join(path_to_repository, FoldersEnum.CVS_DATA)
         return os.path.isdir(path_to_cvs_data)
 
-    @staticmethod
-    def enumerate_directory(directory: str) -> str:
-        for file in os.listdir(directory):
-            full_path = os.path.join(directory, file)
-            if os.path.isdir(full_path):
-                yield from CVS.enumerate_directory(full_path)
-            else:
-                yield file
-
     def _initialize_head(self):
-        self.head = Head.initialize_from_reference(os.path.join(self.path_to_repository,
-                                                                FoldersEnum.CVS_DATA,
-                                                                'HEAD'))
+        path_to_cvs_data = os.path.join(self.path_to_repository, FoldersEnum.CVS_DATA)
+        self.head = CVSStorage.read('HEAD', path_to_cvs_data)
+
 
 class Index:
     def __init__(self, directory: str):
-        self.tree = Tree.initialize_from_directory(directory)
         self.directory = directory
-        self.modified: list[CVSObject] = []
-        self.removed: list[CVSObject] = []
+        self.modified: dict[TreeObjectData, bytes] = {}
+        self.removed: dict[TreeObjectData, bytes] = {}
+        self.new: dict[TreeObjectData, bytes] = {}
 
-    def compare_trees(self, index_tree: "Tree", commit_tree: "Tree") -> "TreeComparisonResult":
-        # берем все файлы из текущей дирректории и все файлы из последнего коммита
-        # сравниваем все файлы текущей дирректории и все файлы из последнего коммита
-        # для тех файлов, у которых совпадает хэш, сравниваем содержимое
-        # если файл есть в текущей дирректории, но его нет в предыдущем коммите, значит он был добавлен
-        # если файла нет в текуйщей дирректории, но он есть в последнем коммите, значит он был удален
+    @staticmethod
+    def compare_trees(first: Tree, second: Tree) -> "TreeComparisonResult":
+        in_first: dict[TreeObjectData, bytes] = {}
+        in_second: dict[TreeObjectData, bytes] = {}
         different: dict[TreeObjectData, bytes] = {}
-        in_index: dict[TreeObjectData, bytes] = {}
-        in_commit: dict[TreeObjectData, bytes] = {}
-        res = TreeComparisonResult(in_index, in_commit, different)
+        res = TreeComparisonResult(in_first, in_second, different)
 
-        path_to_index = os.path.join(self.directory, FoldersEnum.INDEX)
-        commit_objects_data = commit_tree.children.keys()
-        for index_child_data in index_tree.children:
-            if index_child_data not in commit_objects_data:
-                # current is a new file
-                in_index[index_child_data] = index_tree.children[index_child_data]
-            elif index_tree.children[index_child_data] != commit_tree.children[index_child_data]:
-                # current was changed
-                if index_child_data.type == Tree:
-                    index_subtree = CVSStorage.read_object(index_tree.children[index_child_data], Tree, path_to_index)
-                    index_subtree = Tree.deserialize(index_subtree)
-                    commit_subtree = CVSStorage.read_object(commit_tree.children[index_child_data], Tree, self.directory)
-                    commit_subtree = Tree.deserialize(commit_subtree)
-                    comp_result = self.compare_trees(index_subtree, commit_subtree)
-                    res.extend(comp_result)
+        equal: dict[TreeObjectData, bytes] = {}
+        second_tree_objects_data = second.children.keys()
+        for first_tree_object_data in first.children:
+            if first_tree_object_data not in second_tree_objects_data:
+                # current object is new
+                in_first[first_tree_object_data] = first.children[first_tree_object_data]
+            elif first.children[first_tree_object_data] != second.children[first_tree_object_data]:
+                # current object was changed
+                if first_tree_object_data.object_type == Tree:
+                    first_subtree = Tree.initialize_from_directory(first_tree_object_data.path)
+                    second_subtree = Tree.deserialize(CVSStorage.read_object(
+                        second.children[first_tree_object_data],
+                        Tree,
+                        first_tree_object_data.path))
+                    comp_res = Index.compare_trees(first_subtree, second_subtree)
+                    res.extend(comp_res)
+                elif first_tree_object_data.object_type == Blob:
+                    different[first_tree_object_data] = first.children[first_tree_object_data]
                 else:
-                    different[index_child_data] = index_tree.children[index_child_data]
+                    equal[first_tree_object_data] = first.children[first_tree_object_data]
+        in_second.update(map(lambda data: (data, second.children[data]),
+                             filter(lambda data: data not in first.children, second.children)))
 
-        return TreeComparisonResult(in_index, in_commit, different)
+        return res
+
+    def update_index(self, commit: "Commit"):
+        comp_res = Index.compare_trees(Tree.initialize_from_directory(self.directory), commit.tree)
+        self.new = comp_res.in_first
+        self.removed = comp_res.in_second
+        self.modified = comp_res.different
+
+    @staticmethod
+    def _enumerate_directory(directory: str) -> str:
+        for file in os.listdir(directory):
+            full_path = os.path.join(directory, file)
+            if os.path.isdir(full_path):
+                yield from Index._enumerate_directory(full_path)
+            else:
+                yield full_path
+
+    def _enumerate_tree(self, tree: Tree) -> (TreeObjectData, bytes):
+        for obj_data in tree.children.keys():
+            if obj_data.object_type == Tree:
+                other_tree = Tree.deserialize(CVSStorage.read_object(tree.children[obj_data], Tree, self.directory))
+                yield from self._enumerate_tree(other_tree)
+            else:
+                yield obj_data, tree.children[obj_data]
 
 
 @dataclass
 class TreeComparisonResult:
-    def __init__(self,
-                 in_first: dict["TreeObjectData", bytes],
-                 in_second: dict["TreeObjectData", bytes],
-                 different: dict["TreeObjectData", bytes]):
-        self.in_first = in_first
-        self.in_second = in_second
-        self.different = different
+    in_first: dict["TreeObjectData", bytes]
+    in_second: dict["TreeObjectData", bytes]
+    different: dict["TreeObjectData", bytes]
 
     def extend(self, other: "TreeComparisonResult"):
         self.in_first.update(other.in_first)
