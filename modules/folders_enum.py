c94ed6ceb64fea752caf3344275b080a1ae8a262
invis166 <kolesnikovv028@gmail.com>
2021-10-24 00:40:42 +0500

    minor

--- modules/folders_enum.py
+++ modules/folders_enum.py
@@ -5,6 +5,6 @@ class FoldersEnum(str, Enum):
     CVS_DATA = '.cool_cvs/'
     REFS = '.cool_cvs/refs/'
     HEAD = './cool_cvs/HEAD'
-    HEADS = '.cool_cvs/refs/heads'
+    HEADS = '.cool_cvs/refs/heads/'
     OBJECTS = '.cool_cvs/objects/'
     INDEX = '.cool_cvs/index/'
