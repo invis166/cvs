fe57e9a042a2f74ade7d58cd747017f8bd476d45
invis166 <kolesnikovv028@gmail.com>
2021-10-23 19:31:02 +0500

    add HEAD path

--- modules/folders_enum.py
+++ modules/folders_enum.py
@@ -4,6 +4,7 @@ from enum import Enum
 class FoldersEnum(str, Enum):
     CVS_DATA = '.cool_cvs/'
     REFS = '.cool_cvs/refs/'
+    HEAD = './cool_cvs/HEAD'
     HEADS = '.cool_cvs/refs/heads'
     OBJECTS = '.cool_cvs/objects/'
     INDEX = '.cool_cvs/index/'
