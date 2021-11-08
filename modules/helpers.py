048ba56e5c30ac143c6395fbeb078a59afeb9b1f
invis166 <kolesnikovv028@gmail.com>
2021-11-08 17:10:24 +0500

    handle initialization trees with removed files

--- modules/helpers.py
+++ modules/helpers.py
@@ -1,7 +1,7 @@
 import os
 
-from modules.cvs_objects import Tree, TreeObjectData, Blob
-from modules.storage import CVSStorage
+from cvs_objects import Tree, TreeObjectData, Blob
+from storage import CVSStorage
 
 
 class Helpers:
@@ -27,15 +27,30 @@ class Helpers:
     @staticmethod
     def initialize_and_store_tree_from_collection(collection, destination: str) -> Tree:
         tree = Tree()
-        for path in collection:
+        for data in collection:
+            path = data.path
             if os.path.isdir(path):
-                obj = Helpers.initialize_and_store_tree_from_directory(path, destination)
+                if os.path.exists(path):
+                    obj = Helpers.initialize_and_store_tree_from_directory(path, destination)
+                else:
+                    obj = Tree(is_removed=True)
                 obj_data = TreeObjectData(path, Tree)
             else:
-                with open(path, 'rb') as f:
-                    obj = Blob(f.read())
-                    obj_data = TreeObjectData(path, Blob)
-                    CVSStorage.store_object(obj.get_hash().hex(), obj.serialize(), Blob, destination)
+                obj_data = TreeObjectData(path, Blob)
+                if os.path.exists(path):
+                    with open(path, 'rb') as f:
+                        obj = Blob(f.read())
+                        CVSStorage.store_object(obj.get_hash().hex(), obj.serialize(), Blob, destination)
+                else:
+                    obj = Blob(b'', is_removed=True)
             tree.add_object(obj_data, obj.get_hash())
 
         return tree
+
+    @staticmethod
+    def listdir_with_trailing_slash(directory: str):
+        for item in os.listdir(directory):
+            if os.path.isdir(item):
+                yield os.path.join(item, '')
+            else:
+                yield item
