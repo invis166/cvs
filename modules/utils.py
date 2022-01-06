0e091783611ac7b2f5df6c830f965446ee7c00cc
invis166 <kolesnikovv028@gmail.com>
2022-01-06 18:42:01 +0500

    add rmdir; bug fix; remove unused function

--- modules/utils.py
+++ modules/utils.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import difflib
 
 from modules.cvs_objects import Tree, TreeObjectData, Blob
@@ -35,15 +36,16 @@ def initialize_and_store_tree_from_collection(collection, destination: str) -> T
             else:
                 obj = Tree()
                 obj_data = TreeObjectData(path, Tree, is_removed=True)
+            CVSStorage.store_object(obj.get_hash().hex(), obj.serialize(), Tree, destination)
         else:
             if not data.is_removed:
                 obj_data = TreeObjectData(path, Blob)
                 with open(path, 'rb') as f:
                     obj = Blob(f.read())
-                    CVSStorage.store_object(obj.get_hash().hex(), obj.serialize(), Blob, destination)
             else:
                 obj = Blob(b'')
                 obj_data = TreeObjectData(path, Blob, is_removed=True)
+            CVSStorage.store_object(obj.get_hash().hex(), obj.serialize(), Blob, destination)
 
         tree.add_object(obj_data, b'' if obj_data.is_removed else obj.get_hash())
 
@@ -58,37 +60,21 @@ def listdir_with_trailing_slash(directory: str):
             yield item
 
 
-def build_rebase_conflict_file(source_file: str, dest_file: str, source_commit_name: str, dest_commit_name: str):
-    source_file_lines = source_file.split('\n')
-    dest_file_lines = dest_file.split('\n')
-    res = []
-    source_diff = []
-    dest_diff = []
-    while source_file_lines and dest_file_lines:
-        source_line, dest_line = source_file_lines.pop(), dest_file_lines.pop()
-        if dest_line == source_line:
-            res.append(dest_line)
-            if source_diff:
-                res.append(f'====={source_commit_name}=====')
-                res.extend(source_diff)
-                res.append(f'------{dest_commit_name}-----')
-                res.extend(dest_diff)
-            source_diff, dest_diff = [], []
-        else:
-            # как только встретили, продолжаем
-            source_diff.append(source_line)
-            dest_diff.append(dest_line)
-
-    if source_diff:
-        res.append(f'====={source_commit_name}=====')
-        res.extend(source_diff)
-    if dest_diff:
-        res.append(f'------{dest_commit_name}-----')
-        res.extend(dest_diff)
-
-    return res
-
-
 def create_diff_file(path: str, first: list[str], second: list[str]):
     with open(path, 'w') as f:
         f.writelines(difflib.ndiff(first, second))
+
+
+def rmdir(path, ignore=None):
+    if ignore is None:
+        ignore = []
+    for item in os.listdir(path):
+        item = os.path.join(path, item)
+        if os.path.isdir(item):
+            item = os.path.join(item, '')
+        if item in ignore:
+            continue
+        if os.path.isdir(item):
+            shutil.rmtree(item)
+        else:
+            os.remove(item)
