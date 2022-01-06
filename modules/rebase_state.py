f2c27f8504eaea6c82e14a84f99a49c1dc4eed02
invis166 <kolesnikovv028@gmail.com>
2022-01-06 18:40:29 +0500

    add rebase state

---
+++ modules/rebase_state.py
@@ -0,0 +1,15 @@
+from modules.references import Branch
+from modules.cvs_objects import Commit, TreeObjectData
+
+
+class RebaseState:
+    def __init__(self, source_branch: Branch, destination_branch: Branch):
+        self.source_branch = source_branch
+        self.destination_branch = destination_branch
+        self.current_dst_commit = self.source_branch.commit
+        self.current_file: TreeObjectData = None
+        self.applied: set[Commit] = set()
+        self.not_applied: list[Commit] = []
+        self.dst_branch_changed: set[Commit] = set()
+        self.resolved_files: set[TreeObjectData] = set()
+        self.is_conflict = False
